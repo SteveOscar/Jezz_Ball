47044dca114aa98b4421276845d3c706bf4d5d43
martensonbj <martensonbj@gmail.com>
2016-04-05 11:03:39 -0600

    ball tests

--- test/ball-test.js
+++ test/ball-test.js
@@ -1,12 +1,13 @@
 const chai = require('chai'); const assert = chai.assert;
-
 const Ball = require('../lib/ball');
+const Wall = require('../lib/wall');
 
 describe('Ball', function() {
+
   context('with default attributes', function() {
     var ball = new Ball({x: 30, y: 40});
     var radius = 20;
-    var color = 'blue';
+    var color = 'yellow';
     var vx = 2;
     var vy = -2;
 
@@ -32,6 +33,46 @@ describe('Ball', function() {
 
     it('should assign a vy', function(){
       assert.equal(ball.vy, vy);
+    });
+  });
+
+  context('verify location of ball edges', function(){
+    var ball = new Ball({x: 30, y: 40});
+    var wall = new Wall({x: 55, y: 70});
+    var radius = 20;
+    var leftSideOfBall = ball.x - radius;
+    var rightSideOfBall = ball.x + radius;
+    var topOfBall = ball.y - radius;
+    var bottomOfBall = ball.y + radius;
+
+    it('defines a left side of ball', function(){
+      assert.equal(leftSideOfBall, 10);
+    });
+
+    it('defines a right side of ball', function(){
+      assert.equal(rightSideOfBall, 50);
+    });
+
+    it('defines a top of ball', function(){
+      assert.equal(topOfBall, 20);
+    });
+
+    it('defines a bottom of ball', function(){
+      assert.equal(bottomOfBall, 60);
+    });
+  });
+
+  context('checks for hitting a moving horizontal wall', function(){
+    var ball = new Ball({x: 30, y: 40});
+    var wall = new Wall({x: 55, y: 70});
+    
+    function hitsBuildingHorizontalWall(ball) {
+      return (ball.x > wall.x && ball.x < (wall.x + wall.width)	&& wall.building_wall === true);
+    }
+
+    it('returns true if ball hits wall', function(){
+      wall.building_wall = true;
+      assert.equal(hitsBuildingHorizontalWall(ball), true);
     });
   });
 
