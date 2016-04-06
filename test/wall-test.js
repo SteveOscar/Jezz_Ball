d650e5e64fb0da79e153a7c482fe9127c04f290f
Steven Olson <steveoscaro@gmail.com>
2016-04-06 13:53:04 -0600

    add wall helper tests

--- test/wall-test.js
+++ test/wall-test.js
@@ -1,6 +1,8 @@
 const chai = require('chai'); const assert = chai.assert;
+var pry = require('pryjs')
 
 const Wall = require('../lib/wall');
+const WallHelper = require('../lib/wall-helper')
 
 describe('Wall', function() {
   context('with default attributes', function() {
@@ -24,5 +26,18 @@ describe('Wall', function() {
       assert.equal(wall.width, width);
     });
   });
+
+  context('walls can be filtered', function(){
+   var allWalls = [{x: 10, y: 10, orientation: 0}, {x: 20, y: 20, orientation: 0}, {x: 10, y: 10, orientation: 1}, {x: 20, y: 20, orientation: 1} ];
+
+   it('should filter horizontal walls', function(){
+    //  function filteredHorizontal(allWalls) {
+    //    return allWalls.filter(function(wall){return wall.orientation === 0;});
+    //  }
+
+     var result = WallHelper.filteredHorizontal(allWalls);
+     assert.equal(result.length, 2);
+   });
+ });
 
 });
